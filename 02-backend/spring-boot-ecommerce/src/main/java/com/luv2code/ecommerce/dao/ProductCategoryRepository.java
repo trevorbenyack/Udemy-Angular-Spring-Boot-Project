1e0ac643301e0ad225db40070b01a7cdb41d37d1
Trevor Benyack <trevorbenyack@gmail.com>
2021-04-29 22:45:38 -0400

    Adds @CrossOrigin annotation to entities

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -3,6 +3,11 @@ package com.luv2code.ecommerce.dao;
 import com.luv2code.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+// Origin is protocal + hostname + port
+// All three have to match the calling app!
+@CrossOrigin("http://localhost:4200") // This is the server that our angular application is running on
 
 // (collectionResourceRel = "NameOfJsonEntry", </product-category>)
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,10 @@ package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+// Origin is protocal + hostname + port
+// All three have to match the calling app!
+@CrossOrigin("http://localhost:4200") // This is the server that our angular application is running on
 // JpaRepository<MyEntityType, MyIdType>
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
