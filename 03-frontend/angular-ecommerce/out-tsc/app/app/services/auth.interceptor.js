ec5641e005d8fb285424ccf5962bb56187911e76
Trevor Benyack <trevorbenyack@gmail.com>
2021-08-18 14:52:05 -0400

    Completes all videos up to Video 275: 'Release 3.0 - Security - Order History - Secure Front End - Test The App'

---
+++ 03-frontend/angular-ecommerce/out-tsc/app/app/services/auth.interceptor.js
@@ -0,0 +1,38 @@
+import { __awaiter, __decorate } from "tslib";
+import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+let AuthInterceptor = class AuthInterceptor {
+    constructor(oktaAuth) {
+        this.oktaAuth = oktaAuth;
+    }
+    intercept(request, next) {
+        return from(this.handleAccess(request, next));
+        //return next.handle(request);
+    }
+    handleAccess(request, next) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Only add an access token for secured endpoints
+            const securedEndpoints = ['http://localhost:8080/api/orders'];
+            if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+                // get access token
+                const accessToken = yield this.oktaAuth.getAccessToken();
+                // clone the request and add new header with access token
+                // we're cloning b/c the request itself is immutable, so we have
+                // to make a copy first
+                request = request.clone({
+                    setHeaders: {
+                        Authorization: 'Bearer ' + accessToken
+                    }
+                });
+            }
+            // This basically says to go ahead and continue to other interceptors that are in the chain
+            // if there are no interceptors, then just simply make the call to the given rest API
+            return next.handle(request).toPromise();
+        });
+    } // end handleAccess()
+};
+AuthInterceptor = __decorate([
+    Injectable()
+], AuthInterceptor);
+export { AuthInterceptor };
+//# sourceMappingURL=auth.interceptor.js.map
