d9e1ecf7b52b693d38d915a33a6e8c8759e94600
Trevor Benyack <trevorbenyack@gmail.com>
2021-08-18 14:15:20 -0400

    Completes video 268: 'Release 3.0 - Security - Order History - Secure Backend - Protect Endpoints'

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // protect endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated() // only for authenticated users
+                .and()
+                .oauth2ResourceServer() // configures OAuth2 Resource Server support
+                .jwt(); // enables JWT-encoded bearer token support
+
+        // add CORS filters
+        http.cors();
+
+    }
+}
