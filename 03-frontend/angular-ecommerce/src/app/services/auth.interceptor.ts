25ec3d2bb278e6f56d1f8bee1a0849cf89c9706e
Trevor Benyack <trevorbenyack@gmail.com>
2021-08-18 14:44:27 -0400

    Completes all videos up to Video 273: 'Release 3.0 - Security - Order History - Secure Front End - Pass Access Token'

--- 03-frontend/angular-ecommerce/src/app/services/auth.interceptor.ts
+++ 03-frontend/angular-ecommerce/src/app/services/auth.interceptor.ts
@@ -6,11 +6,12 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import {from, Observable} from 'rxjs';
+import {OktaAuthService} from '@okta/okta-angular';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private oktaAuth: OktaAuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
@@ -19,6 +20,28 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-    throw new Error('Method not implemented');
-  }
+
+    // Only add an access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+
+      // get access token
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      // clone the request and add new header with access token
+      // we're cloning b/c the request itself is immutable, so we have
+      // to make a copy first
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken,
+        }
+      });
+    }
+
+    // This basically says to go ahead and continue to other interceptors that are in the chain
+    // if there are no interceptors, then just simply make the call to the given rest API
+    return next.handle(request).toPromise();
+
+  } // end handleAccess()
 }
