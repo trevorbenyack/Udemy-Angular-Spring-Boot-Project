cf0d793f31e78a57782b2385d03cf4d9c46487ce
Trevor Benyack <trevorbenyack@gmail.com>
2021-08-18 14:20:34 -0400

    Completes video 269: 'Release 3.0 - Security - Order History - Secure Backend - Testing The App'

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.luv2code.ecommerce.config;
 
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +21,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // add CORS filters
         http.cors();
+
+        // force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
 
     }
 }
