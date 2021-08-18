5c3964e25dcf7b44a131b562ebc07e9abb8e6532
Trevor Benyack <trevorbenyack@gmail.com>
2021-08-18 15:43:34 -0400

    Completes all videos up to Video 281: 'Secure Communication with HTTPS - Okta Updates - Configuration'

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -6,7 +6,7 @@ export default {
   oidc: {
     clientId: '0oa1h6zg0thIRYeXM5d7',
     issuer: 'https://dev-35767185.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     // Scopes:
     //  openid: required for authentication requests
     scopes: ['openid', 'profile', 'email']
